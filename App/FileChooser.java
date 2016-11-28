44f791eff43fad765d5d8a33f03d308c74700c5c
William <williamaster1@gmail.com>
2016-11-28 15:00:07 -0300

    Criada classe para fileChooser para melhorar a leitura de codigo

---
+++ App/FileChooser.java
@@ -0,0 +1,39 @@
+package App;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+public class FileChooser
+{
+	public String escolherArquivo(String title)
+	{
+		String str = "";
+		JFileChooser chooser = new JFileChooser();
+		File file = new File("C:\\Users\\William\\workspace\\");
+		chooser.setCurrentDirectory(file);
+		chooser.setDialogTitle(title);
+	    if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) 
+	    {
+	       System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
+	       str = chooser.getSelectedFile().getAbsolutePath();
+	    }
+	    return str;
+	}
+	
+	public String escolherDiretorio(String title)
+	{
+		String str = "";
+		JFileChooser chooser = new JFileChooser();
+		File file = new File("C:\\Users\\William\\workspace\\");
+		chooser.setCurrentDirectory(file);
+		chooser.setDialogTitle(title);
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	    if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) 
+	    {
+	       System.out.println("You chose to open this file: " + chooser.getSelectedFile().getAbsolutePath());
+	       str = chooser.getSelectedFile().getAbsolutePath();
+	    }
+	    return str;
+	}
+}
