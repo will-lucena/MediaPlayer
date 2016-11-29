0fa65234ef33ab1fc5b56666bb98855ffd5e3df2
William <williamaster1@gmail.com>
2016-11-29 12:05:14 -0300

    Classes atualizadas, metodos de gerar tela implementados

--- LoginStrategy/LoginModeComum.java
+++ LoginStrategy/LoginModeComum.java
@@ -1,12 +1,14 @@
 package LoginStrategy;
 
+import Interface.TelaNormal;
+
 public class LoginModeComum implements LoginModeStrategy
 {
 
     @Override
     public void gerarTela()
     {
-        
+        new TelaNormal().setVisible(true);
     }
 
     @Override
--- LoginStrategy/LoginModeVip.java
+++ LoginStrategy/LoginModeVip.java
@@ -1,12 +1,14 @@
 package LoginStrategy;
 
+import Interface.TelaVip;
+
 public class LoginModeVip implements LoginModeStrategy
 {
 
     @Override
     public void gerarTela()
     {
-        
+        new TelaVip().setVisible(true);
     }
 
     @Override
